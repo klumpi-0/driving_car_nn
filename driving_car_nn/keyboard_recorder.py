cce9732f6eef6860704613562370a8b8a1f040e9
Jonas <61214005+klumpi-0@users.noreply.github.com>
2022-12-20 13:44:45 +0100

    Keyboard recorder with pynput. Still does not work

---
+++ driving_car_nn/keyboard_recorder.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Key, Listener
+from getkey
+
+class keyboard_recorder:
+    def __init__(self):
+        print("Created a keyboard recorder...")
+
+    def on_press(key):
+        print("Pressed key", key , '{0} pressed'.format(
+            key))
--- driving_car_nn/main.py
+++ driving_car_nn/main.py
@@ -2,21 +2,17 @@
 import time
 
 from scanner import input_scanner
+from keyboard_recorder import keyboard_recorder
 import cv2 as cv
 
 i_s = input_scanner(1000, 1000, 0, 0)
-#test = i_s.record_screen_seconds(100, save_in_class=True)
-#print(type(test[0]))
-#cv.imshow("Name0", test[0])
-#cv.waitKey(0)
-#cv.imshow("Name1", test[99])
-#cv.waitKey(0)
-#cv.imshow("Last Picture", i_s.get_current_save()[59])
-#cv.waitKey(0)
-#cv.destroyAllWindows()
+kr = keyboard_recorder()
+
+for i in range(100):
+    kr.on_press()
 
 #i_s.record_keyboard(2, True, 'p')
 #i_s.record_screen_keyboard(True, "esc", "p")
 #print(len(i_s.current_save))
 #cv.imshow("Name 0", i_s.current_save[4][0])
-i_s.record_screen_keyboard(True, "p", "u ")
+#i_s.record_screen_keyboard(True, "p", "u ")
