e5db8f5ab3d39251fe76887b906af26ba435c8a1
Jonas <61214005+klumpi-0@users.noreply.github.com>
2023-01-06 18:33:56 +0100

    NN is created and trained. Can be saved and loaded. Problems at the point of using it afterwards

--- driving_car_nn/main.py
+++ driving_car_nn/main.py
@@ -7,6 +7,7 @@ from mss import mss
 
 from scanner import input_scanner
 import tf_nn
+import tensorflow as tf
 from keyboard_recorder import keyboard_recorder
 import keyboard
 import cv2 as cv
@@ -16,10 +17,37 @@ import cv2 as cv
 #i_s.record_screen_keyboard_seconds(1000, True)
 #i_s.save_data_at('Assets/', 'input_string')
 
+
+def create_train_model():
+    train_ds, val_ds = tf_nn.load_images('Assets')
+    model = tf_nn.create_nn_model(16)
+    # tf.keras.models.save_model(model=model, filepath='Assets')
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+    model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=20
+    )
+    model.summary()
+    tf.keras.models.save_model(model=model, filepath='Models/NN')
+    return model
+
+
 def main():
     print("Wuhu")
     i_s = input_scanner(1920, 1080, 0, 0)
-    tf_nn.load_images('Assets')
+    model = create_train_model()
+    #model = tf.keras.models.load_model('Models/NN')
+    print(model.summary())
+    data = tf.convert_to_tensor(cv2.cvtColor(cv2.imread('Assets/___x/image_training_grey123.jpg'), cv2.COLOR_BGR2GRAY))
+    print("Shape ", data.shape)
+    output = model(tf.reshape(data, [135, 240]))
+    print(output)
+
 
 
 if __name__ == '__main__':
--- driving_car_nn/tf_nn.py
+++ driving_car_nn/tf_nn.py
@@ -32,6 +32,11 @@ def create_dataset(data, recorder, label_txt):
     iterator = dataset.make_one_shot_iterator()
 
 def load_images(image_path):
+    """
+    Creates an training and validation dataset
+    :param image_path: Place where images are stored in their class folders
+    :return: training dataset and validation dataset
+    """
     batch_size = 20
     image_width = 240
     image_height = 135
@@ -41,6 +46,7 @@ def load_images(image_path):
         subset="training",
         seed=123,
         image_size=(image_height, image_width),
+        color_mode='grayscale',
         batch_size=batch_size
     )
     val_ds = tf.keras.utils.image_dataset_from_directory(
@@ -49,8 +55,21 @@ def load_images(image_path):
         subset="validation",
         seed=123,
         image_size=(image_height, image_width),
+        color_mode='grayscale',
         batch_size=batch_size
     )
     print(train_ds.class_names)
+    return train_ds, val_ds
 
+def create_nn_model(number_classes):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(135, 240)),
+        tf.keras.layers.Rescaling(1./255),
+        tf.keras.layers.Dense(128),
+        tf.keras.layers.Dense(64),
+        tf.keras.layers.Dense(32),
+        tf.keras.layers.Dense(number_classes)
+    ])
+
+    return model
 
